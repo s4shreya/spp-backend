0598afe7bdd62ddbe6153dc6ad605f9fa353f081
unknown <s4shreyadhingra@gmail.com>
2023-09-27 19:23:27 +0530

    added routes

---
+++ routes.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form</body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if(url === '/message' && method === "POST") {
+    const body = [];
+    req.on('data', chunk => {
+        console.log("chunk is ", chunk);
+        body.push(chunk);
+    });
+
+    return req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        console.log("parsed body is ", parsedBody);
+        const message = parsedBody.split("=")[1];
+        fs.writeFileSync("message.txt", message);
+    
+        res.statusCode = 302;
+        res.setHeader('location', '/');
+        return res.end();
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write(
+    "<body><p>Hello... this is my first backend code... so excited!!</p></body>"
+  );
+  res.write("</html>");
+  res.end();
+};
+
+module.exports = requestHandler;
--- server.js
+++ server.js
@@ -1,50 +1,7 @@
 const http = require("http");
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  // console.log(req);
+const routes = require("./routes");
 
-  // exits the process
-  // process.exit();
-  const url = req.url;
-  const method = req.method;
-
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form</body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if(url === '/message' && method === "POST") {
-    const body = [];
-    req.on('data', chunk => {
-        console.log("chunk is ", chunk);
-        body.push(chunk);
-    });
-
-    return req.on('end', () => {
-        const parsedBody = Buffer.concat(body).toString();
-        console.log("parsed body is ", parsedBody);
-        const message = parsedBody.split("=")[1];
-        fs.writeFileSync("message.txt", message);
-    
-        res.statusCode = 302;
-        res.setHeader('location', '/');
-        return res.end();
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write(
-    "<body><p>Hello... this is my first backend code... so excited!!</p></body>"
-  );
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(8080);
